c4e3d887b475e6f8895673e7010950b72a3dfef4
Abdulrahman <10939@holbertonstudents.com>
2025-05-04 07:53:14 +0000

    g

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: prints alphabets
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: prints alphabets
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char num;
 for (num = 0; num < 10; num++)
 {
-    putchar(num + '0');
+putchar(num + '0');
 }
 putchar('\n');
 return (0);
