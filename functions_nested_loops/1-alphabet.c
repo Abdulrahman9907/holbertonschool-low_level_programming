213ae1b2ef2a14a648a1e2662a903c0b6a7ebcde
Abdulrahman <10939@holbertonstudents.com>
2025-05-05 14:59:43 +0000

    gg

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* print_alphabet - prints alphabets in lowercase with a new line
+*/
+void print_alphabet(void)
+{
+char alphabet[27] = "abcdefghijklmnopqrstuvwxyz\n";
+_putchar(alphabet);
+}
