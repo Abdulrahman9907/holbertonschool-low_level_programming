99c862dad411724d3fc5ebebd189e58b8f9ffefe
Abdulrahman <10939@holbertonstudents.com>
2025-05-06 00:36:15 +0000

    g

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
+   #include <stdio.h>
+   #include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Description: Prints through write function which is system level
-*
-* Return: Always 0 (Success)
+  * main - Entry point   
+ *     
+ * Description: Prints through write function which is system level
+*   
+* Return: Always 0 (Success)  
 */
 int main(void)
 {
 write(1, "_putchar\n", 9);
-return (0);
+return (0)
 }
