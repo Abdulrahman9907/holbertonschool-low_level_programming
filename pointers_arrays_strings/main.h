706653043593bd4ec4a2e11be8d5ce3a3d706de3
Abdulrahman <10939@holbertonstudents.com>
2025-05-11 19:33:34 +0000

    g

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void print_array(int *a, int n);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 
 #endif /* MAIN_H */
