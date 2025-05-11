51c5943724b218b9f9fa72baab29fe4a1e3873c3
Abdulrahman <10939@holbertonstudents.com>
2025-05-11 02:54:09 +0000

    g

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * _puts - Prints a string followed by a new line .
+ * _puts - Prints a string followed by a new line.
  * @str: The string to print.
  */
 void _puts(char *str)
 {
 while (*str)
 {
-putchar(*str);
+write(1, str, 1); // Write one character at a time to stdout
 str++;
 }
-putchar('\n');
+write(1, "\n", 1); // Write a newline character
 }
