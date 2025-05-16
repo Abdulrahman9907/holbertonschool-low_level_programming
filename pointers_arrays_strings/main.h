229f1ac2da9677e9c25e8b85537d2b0b39548b7d
Abdulrahman <10939@holbertonstudents.com>
2025-05-16 21:49:43 +0000

    g

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,4 +19,9 @@ char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+
+
 #endif /* MAIN_H */
