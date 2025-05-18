ad54f73acd00c58adc0745b04a1424e8583daa97
Abdulrahman <10939@holbertonstudents.com>
2025-05-18 14:31:34 +0000

    g

--- recursion/main.h
+++ recursion/main.h
@@ -1,8 +1,10 @@
-#ifndef MAIN_H
+
+0;276;0c#ifndef MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif /* MAIN_H */
