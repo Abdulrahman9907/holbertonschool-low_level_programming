ac7574bb47e71fbdcd18bdb679597cf02b953bbc
Abdulrahman <10939@holbertonstudents.com>
2025-05-25 23:02:30 +0300

    g

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a structure representing a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void print_dog(struct dog *d);
+
+#endif /* DOG_H */
