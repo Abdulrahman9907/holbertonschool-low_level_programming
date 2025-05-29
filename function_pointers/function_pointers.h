149ec6b8f097ecc98f9b28d20078605a9e2dac2b
Abdulrahman <10939@holbertonstudents.com>
2025-05-29 16:52:01 +0300

    g

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
-#ifndef HEADERFILE_H
-#define HEADERFILE_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#define NULL ((void *)0)
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-
-#endif
+#endif /* FUNCTION_POINTERS_H */
