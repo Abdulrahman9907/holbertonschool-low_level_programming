cbbaed0c3a8d5fef58e21bf565b3e1db5007d438
Abdulrahman <10939@holbertonstudents.com>
2025-06-14 22:00:05 +0300

    g

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,31 +9,56 @@
 *
 * Return: Address of the new node, or NULL if it failed.
 */
+dlistint_t *create_new_node(int n);
+dlistint_t *insert_at_head(dlistint_t **h, dlistint_t *new_node);
+dlistint_t *insert_at_index(dlistint_t **h, unsigned int idx, dlistint_t *new_node);
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *current;
-unsigned int i = 0;
+dlistint_t *new_node;
 
 if (!h)
 return (NULL);
 
-new_node = malloc(sizeof(dlistint_t));
+new_node = create_new_node(n);
+if (!new_node)
+return (NULL);
+
+if (idx == 0)
+return insert_at_head(h, new_node);
+
+return insert_at_index(h, idx, new_node);
+}
+
+dlistint_t *create_new_node(int n)
+{
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
 if (!new_node)
 return (NULL);
 
 new_node->n = n;
+new_node->prev = NULL;
+new_node->next = NULL;
 
-if (idx == 0)
+return new_node;
+}
+
+dlistint_t *insert_at_head(dlistint_t **h, dlistint_t *new_node)
 {
 new_node->prev = NULL;
 new_node->next = *h;
 if (*h)
 (*h)->prev = new_node;
 *h = new_node;
-return (new_node);
+
+return new_node;
 }
 
-current = *h;
+dlistint_t *insert_at_index(dlistint_t **h, unsigned int idx, dlistint_t *new_node)
+{
+dlistint_t *current = *h;
+unsigned int i = 0;
+
 while (current && i < idx - 1)
 {
 current = current->next;
@@ -54,5 +79,5 @@ current->next->prev = new_node;
 
 current->next = new_node;
 
-return (new_node);
+return new_node;
 }
