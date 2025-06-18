99bf472730ddccff35cb8895a673e886346896db
Abdulrahman <10939@holbertonstudents.com>
2025-06-18 11:11:12 +0300

    g

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 200809L
 #include "main.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -20,21 +20,21 @@ char buffer[1024];
 
 if (argc != 3)
 {
-fprintf(stderr, "Usage: cp file_from file_to\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 {
-fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
 fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 
@@ -42,7 +42,7 @@ do {
 r = read(fd_from, buffer, 1024);
 if (r == -1)
 {
-fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 if (r > 0)
@@ -50,7 +50,7 @@ if (r > 0)
 w = write(fd_to, buffer, r);
 if (w == -1)
 {
-fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
@@ -58,12 +58,12 @@ exit(99);
 
 if (close(fd_from) == -1)
 {
-fprintf(stderr, "Error: Can't close fd %d\n", fd_from);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
 if (close(fd_to) == -1)
 {
-fprintf(stderr, "Error: Can't close fd %d\n", fd_to);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
 
